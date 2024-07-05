ccc94eb1349e85aa78d9e7f7a8ea83c4bebd776e
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-07-05 11:55:30 +0200

    tilpasset ARP

---
+++ locales/da.lua
@@ -0,0 +1,51 @@
+Language = Language or {}
+Language['da'] = { -- Danish
+
+    -- Menu Titles
+    comm_service = 'Samfundstjeneste',
+    give_comms = 'Tildel Samfundstjeneste',
+    give_comms_desc = 'Tildel samfundstjeneste til en spiller.',
+    manage_comms = 'Administrer Samfundstjeneste',
+    manage_comms_desc = 'Administrer samfundstjeneste for en spiller.',
+    remove_comms = 'Fjern Samfundstjeneste?',
+    remove_comms_desc = 'Er du sikker på, at du vil fjerne denne spillers samfundstjeneste?',
+
+    player_id = 'Spiller ID: %s',
+    task_amount = 'Antal Opgaver:',
+    refresh = 'Opdater',
+    go_back = 'Gå Tilbage',
+    click_to_remove = 'Opgaver Tilbage: %s (Klik for at Fjerne)',
+
+    -- TextUI Labels
+    dig_hole = '[E] - Grav Hul',
+
+    -- Progress Bars
+    digging = 'Graver Hul...',
+
+    -- Notifications
+    permission_denied = 'Du har ikke tilladelse til at bruge denne kommando.',
+    restricted_area = 'Du er i et begrænset område, forlad venligst omgående.',
+    player_not_found = 'Spiller ID ikke online.',
+    unable_to_give = 'Kan ikke tildele samfundstjeneste.',
+    unable_to_remove = 'Kan ikke fjerne samfundstjeneste.',
+    silly_goose = 'Du kan ikke tildele dig selv samfundstjeneste.',
+    not_finished = 'Du har ikke afsluttet din samfundstjeneste.',
+
+    comms_given = 'Spiller ID: %s er blevet tildelt %s opgave(r).',
+    comms_removed = 'Samfundstjeneste er blevet fjernet.',
+
+    starting_comms = 'Du er blevet tildelt %s opgave(r) af samfundstjeneste.',
+    tasks_remaining = 'Opgaver Tilbage: %s',
+    finished_comms = 'Du har afsluttet din samfundstjeneste.',
+
+    -- Discord Webhook
+    webhook_title = "%s: %s",
+    webhook_desc = "**Disciplinær:** %s(%s)\n**Mål:** %s(%s)\n**Opgaver:** %s",
+
+    -- Server Console
+    version = '%s | v%s',
+    framework = 'Framework: %s',
+    inventory = 'Inventar: %s',
+    target = 'Mål: %s',
+    cheater = 'Spiller ID: %s er mistænkt for at snyde!'
+}
